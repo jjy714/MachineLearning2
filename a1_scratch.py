5ddb82cde426a2bbd4aea1d810e4c32205ee267b
Jason Jung <jjy714@gmail.com>
2023-11-08 15:33:01 +0900

    Added bucnch

--- a1_scratch.py
+++ a1_scratch.py
@@ -1,79 +1,27 @@
 
-import numpy as np
-import torch
-from torchvision import datasets
-from torch.utils.data import DataLoader
-import time
-import matplotlib.pyplot as plt
+# P10 File Clock angle calculation
 
-trainset = datasets.MNIST(root='./data ', train=True, download=True)
-testset = datasets.MNIST(root='./data ', train=False, download=True)
+# Variable initiation
+time = input("Enter the time in the format of HH:MM: ")
+hour = int(time.split(':')[0])
+minute = int(time.split(':')[1])
 
-train_dataloader = DataLoader(trainset, batch_size=64, shuffle = True)
-test_dataloader = DataLoader(testset, batch_size=64, shuffle = True)
+# Conditions
+if hour > 12:
+    hour = hour - 12
 
-
-
-# Indices for train/val splits : train_idx , valid_idx
-np.random.seed(0)
-val_ratio = 0.1
-train_size = len( trainset )
-indices = list ( range ( train_size ))
-split_idx = int(np. floor ( val_ratio * train_size ))
-np.random.shuffle ( indices )
-train_idx, val_idx = indices[split_idx:], indices[:split_idx]
-
-train_data = trainset.data[train_idx].float()/255.
-train_labels = trainset.targets[train_idx]
-val_data = trainset.data[val_idx].float()/255.
-val_labels = trainset.targets[val_idx]
-test_data = testset.data.float()/255.
-test_labels = testset.targets
+# functions for degree
+def degree(hour,  minute):
+    result = abs(30 * hour - 5.5 * minute)
+    if result > 180:
+        result = 360 - result
+    return result
 
 
-def loop_classification(num):
-    new_train_example = torch.flatten(train_data[num])
-    distance_list = []
-    result_i = 0
-
-    for i in range(len(test_data)):
-        each_test = torch.flatten(test_data[i])
-        # Calculate the Euclidean distance
-        distance = torch.sqrt(torch.sum((each_test - new_train_example)**2))
-        distance_list.append(distance)
-
-    # Find the index of the minimum distance
-    result_i = torch.argmin(torch.tensor(distance_list))
-
-    # Print the results
-    print("the data is classified as: ", test_labels[result_i])
-    print("the correct label is: ", val_labels[num])
-
-    # Check if the classification is correct
-    if test_labels[result_i] == val_labels[num]:
-        print("Classification is correct!")
-    else:
-        print("Classification is incorrect.")
-
-    # Print additional labels
-    print(val_labels[num])
-    print(train_labels[num])
-
-# Assume you have defined train_data, test_data, train_labels, test_labels, val_labels
-loop_classification(0)
-
-
-def broadcasting_classification(num):
-    d_list = []
-    start_time = time.time()
-    for i in test_data:
-        distance = torch.sum((i - train_data[num])**2)
-        d_list.append(distance)
-    lowest_distance = min(d_list)
-    lowest_distance_idx = d_list.index(lowest_distance)
-    print("the data is classified as: ", test_labels[lowest_distance_idx])
-    print("the correct label is: ", val_labels[num])
-    print("The time it takes to run the code is: ", time.time() - start_time)
-
-
-broadcasting_classification(0)
+# Print the result
+for i in range(5):
+    print('{0}:{1} makes {2:0.2f} degrees'.format(hour, minute, degree(hour, minute)))
+    minute = minute + 15
+    if minute > 60:
+        minute = minute - 60
+        hour = hour + 1
--- a2.py
+++ a2.py
@@ -1,20 +1,44 @@
-
 import numpy as np
 import torch
-from torchvision import transforms , datasets
+from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-from torch import nn
 
-#(A) Load CIFAR10 dataset as follows:
-
-
-
-
-
+# (A) Load CIFAR10 dataset as follows:
 
 trainset = datasets.CIFAR10(root='./ data ', train=True, download=True, transform=transforms.ToTensor())
 testset = datasets.CIFAR10(root='./ data ', train=False, download=True, transform=transforms.ToTensor())
+
+
+# (B) Visualize at least one image for each class. You may need to look into how dataset is
+# implemented in PyTorch.
+# shape of the image
+
+
+iters = iter(trainset)
+imgs, labels = iters.next()
+for i in range(10):
+    if label == i:
+        plt.imshow(transforms.ToPILImage()(iters.next()[0]))
+    label = labels[i]
+    # for img, label in trainset
+
+
+plt.show()
+
+# label names
+# take first image
+# image = data_batch_1['data'][0]
+# # take first image label index
+# label = data_batch_1['labels'][0]
+# # Reshape the image
+# image = image.reshape(3,32,32)
+# # Transpose the image
+# image = image.transpose(1,2,0)
+# # Display the image
+# plt.imshow(image)
+# plt.title(label_name[label])
+#
 
 # (C) Split the trainset into training set and validation set with 90% : 10% ratio. Implement
 # dataloaders for CIFAR10.
@@ -35,35 +59,62 @@ test_data = testset.data.float() / 255.
 test_labels = testset.targets
 
 
-
 # (D) Choose any two classes. Then, make a SVM classifier (implement a loss function yourself.
 # Do not use PyTorch implementations of loss functions.) and its training/validation/evaluation
 # code to perform binary classification between those two classes.
-class SVM():
-    def __init__(self, train_data, train_labels, test_data, test_labels):
-        self.train_data = train_data
-        self.train_labels = train_labels
-        self.test_data = test_data
-        self.test_labels = test_labels
+
+# class = 0, 2
+
+
+class SVM:
+    def __init__(self):
+        self.w = torch.randn(3072, 1, requires_grad=True)
+        self.b = torch.randn(1, requires_grad=True)
+        self.gamma = 10
+        self.ex_class1 = 0
+        self.ex_class2 = 2
 
     def train(self, X, Y):
-        w = torch.randn(3072, 1, requires_grad=True)
-        b = torch.randn(1, requires_grad=True)
-        gamma = 10
-        loss = [Y[i]*(X[i]*w+b)>=1 for i in range(len(X))]
-        obj = torch.min(0.5 * torch.norm(w) ** 2 + gamma * torch.sum(loss), torch.tensor(0.0))
+        for i in X:
+            hinge_loss = torch.max(0, 1 - Y[self.ex_class2] * (i @ self.w + self.b))
+
+
+        regularization_term = 0.5 * torch.norm(self.w) ** 2
+        obj = regularization_term + self.gamma * torch.sum(hinge_loss)
 
         return obj
 
+    def predict(self, X):
+        pass
 
-# (E) Train for 10 epochs with batch size 64.
-train_dataloader = DataLoader(train_data, batch_size=64, shuffle=True)
-test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+    def accuracy(self, X, Y):
+        pass
 
-num_of_epochs = 10
 
-for epoch in range(num_of_epochs):
-    SVM.train(train_dataloader,test_dataloader)
+
+#
+# # Instantiate the SVM model
+# svm_model = SVM()
+#
+# # Example usage
+# X_train = torch.randn((100, 3072))  # Replace with your actual training data
+# Y_train = torch.randint(0, 2, (100, 1)) * 2 - 1  # Binary labels (-1 or 1)
+#
+# # Train the SVM
+# loss = svm_model.train(X_train, Y_train)
+#
+# # Perform optimization steps here (missing in the provided code)
+# # Use torch.optim and optimizer.step() to update weights based on gradients
+#
+#
+# # (E) Train for 10 epochs with batch size 64.
+# train_dataloader = DataLoader(train_data, batch_size=64, shuffle=True)
+# test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+#
+# num_of_epochs = 10
+
+# for epoch in range(num_of_epochs):
+#     SVM.train(train_dataloader,test_dataloader)
 
 
 # (F) Perform data normalization. You may need to look into how to use datasets in PyTorch.
@@ -76,4 +127,3 @@ for epoch in range(num_of_epochs):
 # (I) Try to obtain find optimal hyperparameters.
 
 # (J) What is the final test accuracy?
-
