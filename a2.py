56ce5dfc64cf8a90edbdae382f3e92141d22c285
Jason Jung <jjy714@gmail.com>
2023-11-15 15:23:11 +0900

    Little bit fixed

--- a2.py
+++ a2.py
@@ -57,30 +57,56 @@ test_label = testset.targets
 # class = 0, 2
 
 class SVM:
-    def __init__(self, train_data, train_label):
+    def __init__(self, title, train_data, train_label, class1):
+        self.title = title
         self.w = torch.randn(3072, 1)
         self.b = torch.randn(1)
-        self.y_prime = torch.where(train_label == 0, -1, 1)
+        self.x = train_data
+        self.y = train_label
+        self.class1 = class1
+        self.y_prime = torch.where(train_label == class1, -1, 1)
         self.C = 1
-    def hingeloss(self, x, y):
+
+    def hingeloss(self):
         # Regularizer term
         w = self.w
         b = self.b
+        x = self.x
+        y = self.y
 
-        reg = 0.5 * (w * w)
-
-        for i in range(x.shape[0]):
+        reg = 0.5 * torch.norm(w) ** 2
+        # opt_term = y * (torch.matmul(x, self.w) + self.b)
+        # x.shape[0]
+        print("HINGE LOSS for", self.title, "with label", self.class1)
+        for i in range(100):
             # Optimization term
             x_ = x[i].view(3072, -1)
-            opt_term = y[i] * ((w @ x_[i]) + b)
 
+            opt_term = self.y_prime[i] * ((w @ x_[i]) + b)
+            classifier = torch.max(torch.tensor(0.0), 1 - opt_term)
+            if classifier[i] == 0:
+                print("correct")
+                print(opt_term)
+                print(self.y[i])
+                if(self.y[i] != self.class1):
+                    print("wrong label")
+            else:
+                print("incorrect")
             # calculating loss
-            loss = reg + self.C * max(0, 1 - opt_term)
-        return loss[0][0]
+            loss = reg + self.C * torch.max(torch.tensor(0.0), 1 - opt_term)
+        return loss
 
-SVM = SVM(train_data, train_label)
-print(SVM.hingeloss(train_data, train_label))
 
+
+# training
+SVM = SVM("training", train_data, train_label, 2)
+print(SVM.hingeloss())
+# Validation
+SVM = SVM("Validating", val_data, val_label, 4)
+print(SVM.hingeloss())
+# Evaluation
+SVM = SVM("Evaluating", test_data, test_label, 3)
+print(SVM.hingeloss())
 
 # # (E) Train for 10 epochs with batch size 64.
 
@@ -90,14 +116,10 @@ test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
 
 def train_svm(X, y):
     svm = SVM()
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
-    train_loader = DataLoader(list(zip(X_train, y_train)), batch_size=32)
-    val_loader = DataLoader(list(zip(X_val, y_val)), batch_size=32)
 
     for epoch in range(10):  # 훈련은 10 에포크 진행합니다.
-        for X_batch, y_batch in train_loader:
-            output = svm.forward(X_batch)
-            loss = svm.hinge_loss(output, y_batch)
+        for X_batch, y_batch in train_dataloader:
+            loss = svm.hinge_loss(X_batch, y_batch)
             loss.backward()
             svm.update_params()  # 업데이트
 
