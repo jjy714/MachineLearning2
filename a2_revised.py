5f6d64ea6707d3ccaa5fae961fa90c54040656a3
jason <jjy714@gmail.com>
2024-02-16 23:55:27 +0900

    SVM class finished
    Signed-off-by: jason <jjy714@gmail.com>

--- a2_revised.py
+++ a2_revised.py
@@ -4,6 +4,8 @@ from torchvision import transforms, datasets
 from torch.utils.data import DataLoader, random_split
 import matplotlib.pyplot as plt
 from torch.optim import SGD
+import torch.nn
+import time
 
 trainset = datasets.CIFAR10(root='./CIFARdata', train=True, download=True, transform=transforms.ToTensor())
 testset = datasets.CIFAR10(root='./CIFARdata', train=False, download=True, transform=transforms.ToTensor())
@@ -44,38 +46,25 @@ train_dataset, val_dataset = random_split(trainset, [train_size, val_size])
 
 # data shape[0] = (1, 3, 32, 32)
 # label shape[0] = (1)
-trainloader = DataLoader(train_dataset, shuffle=True)
+
+trainloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 valloader = DataLoader(val_dataset, shuffle=True)
 testloader = DataLoader(testset, shuffle=False)
 
 anyclass1, anyclass2 = 3, 7
 
 
-
-# torch.where
-
-# if yi >=1 -> target1
-# if yi <= -1 -> target2
-
-# if ans (1 or -1) and data label calcuation results in a correct answer, pass
-# else, if the result is not matching, adjust the w and b value.
-
-
-# update w and b to calculate to a result of 1
 class svm(torch.nn.Module):
     def __init__(self):
         super(svm, self).__init__()
+        self.fc = torch.nn.Linear(32, 1)
 
-    def classifier(self, data):
-        w, b = self.parameters()
-        result = data * w + b
-        return torch.sign(result)
+    def forward(self, x):
+        fd = self.fc(x)
+        return fd
 
     def hingeloss(self, data, label):
-        w, b = self.parameters()
-        zero = self.zero
-
-        regularizer = 0.5 * w * w
+        zero = torch.zeros(1)
         # for i in range(data_size):
         #     ans = -1
         #     if data[i] is not ans_label1 or ans_label2:
@@ -86,58 +75,73 @@ class svm(torch.nn.Module):
         #             result = torch.max(zero, 1 - ans * (w.T * data - b))
         #         if data[i] is ans_label2:
         #             result = torch.max(zero, 1 - ans * (w.T * data - b))
-        term = label * (w.T * data) + b
-        loss = regularizer + self.c * torch.max(zero, 1 - term)
-
+        loss = torch.mean(torch.max(zero, 1 - label * data))
         return loss
 
-    def parameters(self):
-        w = torch.rand()
-        b = torch.rand()
-        return self.w, self.b
 
 
 model = svm()
 epoch = 10
 batch_size = 64
-learning_rate = 0.1
-
+learning_rate = 0.001
 optimizer = SGD(model.parameters(), lr=learning_rate)
-loss_fn = model.hingeloss()
-optimizer.step()
 
 
-def train():
+
+def train(model, optimizer):
     size = train_size
+    n = 1
+    model.train()
 
-    for epochs in range(epoch):
-        print("Epoch: ", epochs)
-        for X, y in trainloader:
-            if y == anyclass1:
-                y = -1
-            elif y == anyclass2:
-                y = 1
+    for batch, (X, y) in enumerate(trainloader):
+        label=0
+        for i in y:
+            if i == anyclass1:
+                label = -1
+            elif i == anyclass2:
+                label = 1
             else:
                 continue
-            optimizer.zero_grad()
-            prediction = svm.classifier(X)
-            loss = model.hingeloss(prediction, y)
-            loss.backward()
-            optimizer.step()
-        print("The loss is ", loss)
+        if label != anyclass1 and label != anyclass2: continue
+        optimizer.zero_grad()
+        prediction = model(X)
+        loss = model.hingeloss(prediction, y)
+        loss.backward()
+        optimizer.step()
+        if batch % batch_size == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"Training loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-def validation():
+
+def validation(model, optimizer):
     size = val_size
+    n = 1
+    for batch, (X, y) in enumerate(valloader):
+        start_time = time.time()
+        if y == anyclass1:
+            y = -1
+        elif y == anyclass2:
+            y = 1
+        else:
+            continue
+        optimizer.zero_grad()
+        prediction = model.classifier(X)
+        loss = model.hingeloss(prediction, y)
+        loss.backward()
+        optimizer.step()
+        if batch % batch_size == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"Training loss: {loss:>7f}  [{current:>5d}/{size:>5d}] at step{n}")
 
-    for epochs in range(epoch):
-        for X, y in valloader:
-            optimizer.zero_grad()
-            prediction = svm.classifier(X)
-            loss = model.hingeloss(X, y)
-            loss.backward()
-            optimizer.step()
-        return loss
+    n += 1
 
 
-train()
+
+for epoch in range(epoch):
+    n = 1
+    start_time = time.time()
+    print(f'Epoch {epoch + 1}')
+    train(model, optimizer)
+    print(f'Time took for step [{n}]: {(time.time() - start_time) / 60:>0.2f} mins')
+    n += 1
